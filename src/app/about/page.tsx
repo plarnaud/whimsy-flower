3965d3915461d49fc656e3aac9785aad7ffc892f
plarnaud <arnaud.planche@bontemps.games>
2026-02-07 17:25:33 -0500

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
 import PageScaffold from "@/components/pageScaffold";
+import MeetWhimsy from "@/components/meetWhimsy";
+import SubPageHeader from "@/components/subPageHeader";
+import { useEffect, useMemo, useState } from "react";
+import { InquireFormSection } from "@/components/inquireForm";
 
 export default function AboutPage() {
-  return <PageScaffold />;
+  return (
+    <PageScaffold>
+      <SubPageHeader
+        title="About"
+        imgSrc="/home-lander-section-bg.png"
+        imgAlt="Wedding bouquet with white and blush roses and greenery"
+      />
+
+      <MeetWhimsy />
+
+      <InquireFormSection />
+    </PageScaffold>
+  );
 }
